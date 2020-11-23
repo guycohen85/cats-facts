f39d17b70dd0b99de54bb7b81590648b6a619bfb
guy <guycohen85@gmail.com>
2020-11-23 18:26:37 +0200

    Add frontend Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+window.Vue = require('vue');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const app = new Vue({
+    el: '#app',
+});
