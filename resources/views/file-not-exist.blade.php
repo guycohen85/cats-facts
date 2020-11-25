c87155d17cccc76f2f02e67694d73f6ead02e8c0
guy <guycohen85@gmail.com>
2020-11-25 12:52:20 +0200

    Add views

---
+++ resources/views/file-not-exist.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/app.css" rel="stylesheet">
+        <title>Cat Facts</title>
+    </head>
+    <body>
+        <h1 class="m-3 text-xl">File not exist, Please create one from <a class="text-indigo-500" href="/">Cats Facts</a></h1>
+    </body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,22 @@
     </head>
     <body>
         <div id="app">
-            <div class="container mx-auto">
-                <h1>sdfgsdfg</h1>
+            <header class="bg-white shadow">
+                <div class="flex max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                  <h1 class="text-3xl font-bold leading-tight text-gray-900">
+                    CATS FACTS
+                  </h1>
+                </div>
+              </header>
+              <main>
+                <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                    <div class="lg:flex">
+                        <facts-list class="lg:w-1/4 lg:mr-5 mb-5"></facts-list>
+                        <details-panel class="lg:flex-1 mb-5"></details-panel>
+                    </div>
+                    <div class="flex flex-row-reverse object-right text-indigo-500 underline"><a target="_blank" href="/cats-facts">view JSON file</a></div>
+                </div>
+              </main>
             </div>
         </div>
         <script src="/js/app.js"></script>
