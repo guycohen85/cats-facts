1efc50741f69d6993aff51a2fdd537da902c0837
guy <guycohen85@gmail.com>
2020-11-25 12:53:45 +0200

    Main app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,19 @@
+import store from "./store";
+import FactsList from "./components/FactsList.vue";
+import DetailsPanel from "./components/DetailsPanel.vue";
+
 window.Vue = require('vue');
+Vue.component("facts-list", FactsList);
+Vue.component("details-panel", DetailsPanel);
+
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-const app = new Vue({
+
+new Vue({
+    store,
     el: '#app',
+    created() {
+        this.$store.dispatch('loadFacts');
+    }
 });
